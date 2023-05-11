a06f8a485c6d5f66776e3d566be63f320137d7f0
rhartsuiker <r.hartsuiker@st.hanze.nl>
2023-05-11 19:53:28 +0200

    assignment 1 done - need to check pylint

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -5,42 +5,51 @@ __author__ = "Ruben Hartsuiker"
 # Imports
 import sys
 import csv
-import argparse
-
+import argparse as ap
 import multiprocessing as mp
 import numpy as np
 
 
 # Functions
-def get_quality(fastq_file):
-    """Takes a fastq file and calculates the average quality of every base.
-    input: file.fastq
+def convert_to_phred(qual_line):
+    """Takes a quality line from a fastq file and converts it to phred scores.
+    input: quality line from fastq file
     output: ndarray"""
-    with open(fastq_file, 'r') as openfile:
-        lines = openfile.readlines()
-        sum_of_quality = [[10 * np.log(ord(c)) for c in p] for p in lines[3::4]]
-    return np.mean(sum_of_quality, axis=0)
+    return [[10 * np.log(ord(c)) for c in p] for p in qual_line]
 
 
 # Main
 def main(args):
     """main function is called when module is used independently"""
-    with mp.Pool(args.cores) as pool:
-        results = pool.map(get_quality, args.fastq_files)
+    for fastq_file in args.fastq_files:
+        # get all the qual lines
+        qual_lines = fastq_file.readlines()[3::4]
 
-    if args.outfile is not None:
-        with open(args.outfile, 'w', newline='') as outfile:
-            writer = csv.writer(outfile)
-            writer.writerow([""] + args.fastq_files)
-            for i, row in enumerate(np.stack(results, axis=1)):
+        # pool the qual lines and convert them to phred scores
+        with mp.Pool(args.n) as pool:
+            phred_scores = pool.map(convert_to_phred, qual_lines)
+
+        # calc mean over the columns
+        mean_phred_scores = np.mean(phred_scores, axis=0)
+
+        # if outfile was given write to csv else write to commandline
+        if args.csvfile is not None:
+            writer = csv.writer(args.csvfile)
+            writer.writerow([fastq_file.name])
+            for i, row in enumerate(np.stack(mean_phred_scores, axis=0)):
                 writer.writerow([i] + list(row))
-    else:
-        print(results)
+        else:
+            print(fastq_file.name)
+            print(mean_phred_scores)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', type=int, dest='cores', required=True)
-    parser.add_argument('-o', type=str, dest='outfile')
-    parser.add_argument('fastq_files', nargs=argparse.REMAINDER)
-    sys.exit(main(parser.parse_args()))
+    argparser = ap.ArgumentParser(description="Script voor Opdracht 1 van Big Data Computing")
+    argparser.add_argument("-n", action="store", dest="n", required=True, type=int,
+                        help="Aantal cores om te gebruiken.")
+    argparser.add_argument("-o", action="store", dest="csvfile", type=ap.FileType('w', encoding='UTF-8'), required=False, 
+                        help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
+    argparser.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='+', 
+                           help="Minstens 1 Illumina Fastq Format file om te verwerken")
+    args = argparser.parse_args()
+    sys.exit(main(argparser.parse_args()))
