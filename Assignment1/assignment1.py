b90eb61e2937e0f2c8e6a0b5317852ed43209949
rhartsuiker <r.hartsuiker@st.hanze.nl>
2023-05-25 12:56:01 +0200

    commit before pull

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -16,26 +16,34 @@ def get_quality(fastq_file):
     """Takes a fastq file and calculates the average quality of every base.
     input: file.fastq
     output: ndarray"""
-    with open(fastq_file, 'r') as openfile:
+    with open(fastq_file, 'r', encoding='utf8') as openfile:
         lines = openfile.readlines()
-        sum_of_quality = [[10 * np.log(ord(c)) for c in p] for p in lines[3::4]]
+        print("normal\n" + lines)
+        print("splitted\n" + lines[3::4])
+        #sum_of_quality = [[10 * np.log(ord(c)) for c in p] for p in lines[3::4]]
     return np.mean(sum_of_quality, axis=0)
+
+
+def write_to_csv(fastq_files, outfile, results):
+    with open(outfile, 'w', encoding='utf8', newline='') as writefile:
+        writer = csv.writer(writefile)
+        writer.writerow([""] + fastq_files)
+        for i, row in enumerate(np.stack(results, axis=1)):
+            writer.writerow([i] + list(row))
 
 
 # Main
 def main(args):
     """main function is called when module is used independently"""
+    # ~ for fastq_file in args.fastq_files:
+        # ~ with open(args.fastq_files, 'r', encoding='utf8') as openfile:
     with mp.Pool(args.cores) as pool:
         results = pool.map(get_quality, args.fastq_files)
 
-    if args.outfile is not None:
-        with open(args.outfile, 'w', newline='') as outfile:
-            writer = csv.writer(outfile)
-            writer.writerow([""] + args.fastq_files)
-            for i, row in enumerate(np.stack(results, axis=1)):
-                writer.writerow([i] + list(row))
-    else:
-        print(results)
+    # ~ if args.outfile is not None:
+        # ~ write_to_csv(args.fastq_files, args.outfile, results)
+    # ~ else:
+        # ~ print(results)
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BDC
 course big data computing assignments
+
+## Assignment 1
+usage: assignment1.py [-h] -n CORES [-o OUTFILE] ...
+
+example: ~/assignment1.py -n 1 rnaseq_selection.fastq
+    
